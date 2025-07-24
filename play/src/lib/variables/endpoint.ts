755b7237f5a72a8542009b521ba20e26fd26f1a4
Muhamad Aqil Maulana <aqilalumindo@gmail.com>
2025-07-24 19:07:35 +0700

    update

--- play/src/lib/variables/endpoint.ts
+++ play/src/lib/variables/endpoint.ts
@@ -1,4 +1,4 @@
 const isDevelopment = process.env.NODE_ENV === "development";
 
-export const endpointServer = isDevelopment ? "http://localhost:3001" : process.env.SERVER_API_URL as string;
+export const endpointServer = isDevelopment ? "http://localhost:3001" : "https://placeholder";
 export const endpointApiRoute = isDevelopment ? "http://localhost:3000" : "https://quiz-game-play.vercel.app";
