8e4069d99d69e3dfbdc4aa8f2b94791d84f1d029
Muhamad Aqil Maulana <aqilalumindo@gmail.com>
2025-07-25 07:53:07 +0700

    Fix error

--- play/src/app/(admin)/layout.tsx
+++ play/src/app/(admin)/layout.tsx
@@ -1,14 +1,19 @@
 import { auth } from "@/auth";
-import { redirect} from "next/navigation";
+import { redirect } from "next/navigation";
 import React from "react";
 
-export default async function AdminLayout({ children }:{children:React.ReactNode}){
-    const session = await auth();
-    const user = session?.user;
-    const isAllowed = user?.roles.includes("admin")
+export const dynamic = "force-dynamic";
+export default async function AdminLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const session = await auth();
+  const user = session?.user;
+  const isAllowed = user?.roles.includes("admin");
 
-    // TODO : Nanti tambahin unauthorized page
-    if(!isAllowed) redirect("/login")
+  // TODO : Nanti tambahin unauthorized page
+  if (!isAllowed) redirect("/login");
 
-    return children
+  return children;
 }
