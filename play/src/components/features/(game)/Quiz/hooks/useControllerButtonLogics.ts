33112d0a859d5781a565b5533323c3b0b5af18dd
Muhamad Aqil Maulana <aqilalumindo@gmail.com>
2025-07-26 08:30:55 +0700

    Stopwatch

--- play/src/components/features/(game)/Quiz/hooks/useControllerButtonLogics.ts
+++ play/src/components/features/(game)/Quiz/hooks/useControllerButtonLogics.ts
@@ -3,16 +3,18 @@ import { useQuizData } from "../Provider";
 export function useControllerButtonLogics() {
   const { setQuizState, stopTimer, stopwatch } = useQuizData();
 
-  const { seconds, minutes } = stopwatch;
+  const { seconds, minutes, pause:pauseStopwatch } = stopwatch;
 
   const pauseHandler = () => {
     setQuizState((prev) => ({ ...prev, isPausedUser: true }));
     stopTimer();
+    pauseStopwatch();
   };
 
   const configHandler = () => {
     setQuizState((prev) => ({ ...prev, isConfig: true }));
     stopTimer();
+    pauseStopwatch();
   };
 
   const stopwatchTime = `${String(minutes).padStart(2, "0")}:${String(
